738b709cc0a74b1c91a117e43e4fc36b15f92cee
Eric Bower <me@erock.io>
2022-05-10 10:56:28 -0400

    export react interface hook results

--- src/react.ts
+++ src/react.ts
@@ -5,28 +5,31 @@ import type { LoadingState } from 'robodux';
 import type { QueryState } from './slice';
 import { selectLoaderById, selectDataById } from './slice';
 
-type Data<D = any> = LoadingState & { data: D | null; trigger: () => any };
-type DataPayload<D = any, P = any> = LoadingState & {
+export interface UseApiResult<D = any> extends LoadingState {
+  data: D | null;
+  trigger: () => any;
+}
+export interface UseApiPayloadResult<D = any, P = any> extends LoadingState {
   data: D | null;
   trigger: (p: P) => any;
-};
-type DataPayloadOptional<D = any, P = any> = LoadingState & {
+}
+export interface UseApiOptionalResult<D = any, P = any> extends LoadingState {
   data: D | null;
   trigger: (p?: P) => any;
-};
+}
 
 export function useQuery<D = any, S = any, P = any>(
   actionFn: (p: P) => { type: string },
   selector: (s: S) => D,
-): DataPayload<D>;
+): UseApiPayloadResult<D>;
 export function useQuery<D = any, S = any>(
   actionFn: () => { type: string },
   selector: (s: S) => D,
-): Data<D>;
+): UseApiResult<D>;
 export function useQuery<D = any, S = any, P = any>(
   actionFn: (p?: P) => { type: string },
   selector: (s: S) => D,
-): DataPayloadOptional<D> {
+): UseApiOptionalResult<D> {
   const dispatch = useDispatch();
   const data = useSelector(selector);
   const loader = useSelector((s: QueryState) =>
@@ -41,7 +44,7 @@ export function useQuery<D = any, S = any, P = any>(
 
 export function useCache<D = any, S = any>(action: {
   payload: { name: string; key: string };
-}): Data<D> {
+}): UseApiResult<D> {
   const dispatch = useDispatch();
   const data = useSelector((s: S) =>
     selectDataById(s, { id: action.payload.key }),
